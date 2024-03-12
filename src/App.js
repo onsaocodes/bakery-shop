448d4bcd4c7b1514dc0f10daa12527705c64e786
Onome Ogwuda <onomeogwuda@gmail.com>
2024-03-12 18:27:20 +0000

    create logo and nav bar options

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage/LandingPage";
+
 function App() {
-  return;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>{/* <Route path="/" element={<LandingPage />} /> */}</Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="nav__logo">
+        {/* <Link to="/" className="nav__logo-link"> */}
+        <h3 className="nav__logo-text">the bakery.</h3>
+        {/* </Link> */}
+      </div>
+      <ul className="nav__list">
+        <li className="nav__list-item">about</li>
+        <li className="nav__list-item">menu</li>
+        <li className="nav__list-item">shop now</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
